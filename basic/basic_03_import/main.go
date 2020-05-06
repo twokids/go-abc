605c10182e3334c87655bd9d04bac7fb8998c7a3
nanshan03 <734463238@qq.com>
2020-05-06 10:11:37 +0800

    feat：import别名，.，_的区别

--- basic/basic_03_import/main.go
+++ basic/basic_03_import/main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"go-abc/basic/basic_03_import/animal"
 	"go-abc/basic/basic_03_import/life"//重复引用只会有一次
+
+	//. "fmt" 可直接使用fmt包中的函数。但是不建议如此使用，可能导致函数或变量重复
+	//_ "fmt" 仅初始化fmt包中的init
+	//pt "fmt" 使用别名去使用fmt包中的函数
 )
 
 func init()  {
